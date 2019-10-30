42549492846129427022c727347d4691f16a10d5
samuel Nyarkoh <cyelons45@yahoo.com>
2019-10-30 18:10:46 -0400

    Extending dgrid functionalities

---
+++ script.js
@@ -0,0 +1,18 @@
+// require (['dgrid/Grid', 'dojo/domReady!'], function (Grid) {
+//   var data = [
+//     {first: 'Bob', last: 'Barker', age: 89},
+//     {first: 'Vanna', last: 'White', age: 55},
+//     {first: 'Pat', last: 'Sajak', age: 65},
+//   ];
+
+//   var grid = new Grid (
+//     {
+//       columns: {
+//         first: 'First Name',
+//         last: 'Last Name',
+//         age: 'Age',
+//       },
+//     },
+//     'grid'
+//   );
+// });
